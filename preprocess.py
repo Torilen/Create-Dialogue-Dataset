f388b4f44c2125a1741617b1120e13ae12e0ca7d
Aniss Bentebib <abentebib@asmodine.com>
2021-06-03 22:07:11 +0200

    debug preprocessor

--- preprocess.py
+++ preprocess.py
@@ -47,51 +47,54 @@ if __name__ == "__main__":
     print(list_subreddit)
 
     while not reach_end and i < args.maxCommentProcessed:
-        stats.total += 1
-        comment = subprocess.check_output('head -{} {}'.format(i, args.decompressedSourceFilePath), shell=True).decode("utf-8")
 
-        if comment == "end\n":
-            reach_end = True
+        comment = subprocess.check_output('sed "{}q;d" {}'.format(i, args.decompressedSourceFilePath), shell=True).decode("utf-8")
+        if not comment == "\n":
+            stats.total += 1
+            if comment == "end\n":
+                reach_end = True
+            else:
+                i += 1
+                print("=================")
+                print(comment)
+                comment_loaded = json.loads(comment)
+                body = comment_loaded["body"]
+
+                is_bad_subreddit = comment_loaded["subreddit"] not in list_subreddit
+                if is_bad_subreddit: stats.bad_subreddit += 1; continue
+                is_a_bot = body.__contains__("I am a bot") or body.__contains__("I'm a bot")
+                if is_a_bot: stats.removed += 1; continue
+                is_deleted = body.__contains__("[deleted]")
+                if is_deleted: stats.deleted += 1; continue
+                is_removed = body.__contains__("[removed]")
+                if is_removed: stats.removed += 1; continue
+
+                is_empty = body.strip() == ""
+                if is_empty: stats.empties += 1; continue
+
+                try:
+                    languages = detect_langs(body[0:50])
+                except:
+                    stats.non_french += 1
+                    continue
+
+                not_french = languages[0].lang != 'fr'
+                if not_french: stats.non_french += 1; continue
+
+                low_french = languages[0].prob < args.frenchThreshold
+                if low_french: stats.low_french += 1; continue
+
+                # Le commentaire est valable
+                comment_id = comment_loaded['id']
+                data = ','.join([i, comment_loaded['author'], "\""+comment_loaded['body']+"\"", comment_loaded['controversiality'],
+                                 comment_loaded['created_utc'], comment_loaded['distinguished'], comment_loaded['id'],
+                                 comment_loaded['parent_id'], comment_loaded['score'], "\""+comment_loaded['subreddit']+"\"",
+                                 comment_loaded['subreddit_id']])
+                os.system("echo \"{}\" >> ./reddit_source_fr_preprocessed.csv".format(data))
+
+                stats.ok += 1
         else:
-            i += 1
-            print("=================")
-            print(comment)
-            comment_loaded = json.loads(comment)
-            body = comment_loaded["body"]
-
-            is_bad_subreddit = comment_loaded["subreddit"] not in list_subreddit
-            if is_bad_subreddit: stats.bad_subreddit += 1; continue
-            is_a_bot = body.__contains__("I am a bot") or body.__contains__("I'm a bot")
-            if is_a_bot: stats.removed += 1; continue
-            is_deleted = body.__contains__("[deleted]")
-            if is_deleted: stats.deleted += 1; continue
-            is_removed = body.__contains__("[removed]")
-            if is_removed: stats.removed += 1; continue
-
-            is_empty = body.strip() == ""
-            if is_empty: stats.empties += 1; continue
-
-            try:
-                languages = detect_langs(body[0:50])
-            except:
-                stats.non_french += 1
-                continue
-
-            not_french = languages[0].lang != 'fr'
-            if not_french: stats.non_french += 1; continue
-
-            low_french = languages[0].prob < args.frenchThreshold
-            if low_french: stats.low_french += 1; continue
-
-            # Le commentaire est valable
-            comment_id = comment_loaded['id']
-            data = ','.join([i, comment_loaded['author'], "\""+comment_loaded['body']+"\"", comment_loaded['controversiality'],
-                             comment_loaded['created_utc'], comment_loaded['distinguished'], comment_loaded['id'],
-                             comment_loaded['parent_id'], comment_loaded['score'], "\""+comment_loaded['subreddit']+"\"",
-                             comment_loaded['subreddit_id']])
-            os.system("echo \"{}\" >> ./reddit_source_fr_preprocessed.csv".format(data))
-
-            stats.ok += 1
+            continue
 
         if stats.total % 10000 == 0:
             print("Processed: " + str(stats.total) + "\n STATS : " + json.dumps(stats.__dict__))

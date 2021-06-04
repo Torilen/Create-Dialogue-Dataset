76bd0c589b7e600da38d1b71265398c4d7dd722e
Aniss Bentebib <abentebib@asmodine.com>
2021-06-04 16:27:13 +0200

    debug preprocessor

--- preprocess.py
+++ preprocess.py
@@ -47,61 +47,63 @@ if __name__ == "__main__":
 
     print(list_subreddit)
     start = time.time()
-    while not reach_end and stats.ok < args.maxCommentProcessed:
+    with open(args.decompressedSourceFilePath, 'r') as source_file:
+        for comment in source_file:
+            if stats.ok < args.maxCommentProcessed:
+                i += 1
+                stats.total += 1
+                if not comment == "\n":
+                    if comment == "end\n":
+                        reach_end = True
+                    else:
+                        comment_loaded = json.loads(comment)
+                        body = comment_loaded["body"]
 
-        comment = subprocess.check_output('sed "{}q;d" {}'.format(i, args.decompressedSourceFilePath), shell=True).decode("utf-8")
-        i += 1
-        stats.total += 1
-        if not comment == "\n":
-            if comment == "end\n":
-                reach_end = True
-            else:
-                comment_loaded = json.loads(comment)
-                body = comment_loaded["body"]
+                        is_bad_subreddit = comment_loaded["subreddit"] not in list_subreddit
+                        if is_bad_subreddit: stats.bad_subreddit += 1; continue
+                        is_a_bot = body.__contains__("I am a bot") or body.__contains__("I'm a bot")
+                        if is_a_bot: stats.removed += 1; continue
+                        is_deleted = body.__contains__("[deleted]")
+                        if is_deleted: stats.deleted += 1; continue
+                        is_removed = body.__contains__("[removed]")
+                        if is_removed: stats.removed += 1; continue
 
-                is_bad_subreddit = comment_loaded["subreddit"] not in list_subreddit
-                if is_bad_subreddit: stats.bad_subreddit += 1; continue
-                is_a_bot = body.__contains__("I am a bot") or body.__contains__("I'm a bot")
-                if is_a_bot: stats.removed += 1; continue
-                is_deleted = body.__contains__("[deleted]")
-                if is_deleted: stats.deleted += 1; continue
-                is_removed = body.__contains__("[removed]")
-                if is_removed: stats.removed += 1; continue
+                        is_empty = body.strip() == ""
+                        if is_empty: stats.empties += 1; continue
 
-                is_empty = body.strip() == ""
-                if is_empty: stats.empties += 1; continue
+                        try:
+                            languages = detect_langs(body[0:50])
+                        except:
+                            stats.non_french += 1
+                            continue
 
-                try:
-                    languages = detect_langs(body[0:50])
-                except:
-                    stats.non_french += 1
+                        not_french = languages[0].lang != 'fr'
+                        if not_french: stats.non_french += 1; continue
+
+                        low_french = languages[0].prob < args.frenchThreshold
+                        if low_french: stats.low_french += 1; continue
+
+                        # Le commentaire est valable
+                        comment_id = comment_loaded['id']
+                        data = ';'.join([str(i), str(comment_loaded['author']), "\""+comment_loaded['body'].replace("`", "'")+"\"", str(comment_loaded['controversiality']),
+                                         str(comment_loaded['created_utc']), str(comment_loaded['distinguished']), str(comment_loaded['id']),
+                                         str(comment_loaded['parent_id']), str(comment_loaded['score']), "\""+comment_loaded['subreddit']+"\"",
+                                         str(comment_loaded['subreddit_id'])])
+                        #subprocess.check_output("echo `{}` >> ./reddit_source_fr_preprocessed.csv".format(data), shell=True)
+                        with open("./reddit_source_fr_preprocessed.csv", 'a') as file:
+                            file.write(data+"\n")
+                        file.close()
+                        stats.ok += 1
+                        if stats.total % 10000 == 0:
+                            print("=====================")
+                            end = time.time()
+                            print("Processed: " + str(stats.total) + "\n STATS : " + json.dumps(stats.__dict__) + "TIME : " + str((end-start)/60) + "min", flush=True)
+                            start = time.time()
+                else:
+                    stats.empties += 1
                     continue
-
-                not_french = languages[0].lang != 'fr'
-                if not_french: stats.non_french += 1; continue
-
-                low_french = languages[0].prob < args.frenchThreshold
-                if low_french: stats.low_french += 1; continue
-
-                # Le commentaire est valable
-                comment_id = comment_loaded['id']
-                data = ';'.join([str(i), str(comment_loaded['author']), "\""+comment_loaded['body'].replace("`", "'")+"\"", str(comment_loaded['controversiality']),
-                                 str(comment_loaded['created_utc']), str(comment_loaded['distinguished']), str(comment_loaded['id']),
-                                 str(comment_loaded['parent_id']), str(comment_loaded['score']), "\""+comment_loaded['subreddit']+"\"",
-                                 str(comment_loaded['subreddit_id'])])
-                #subprocess.check_output("echo `{}` >> ./reddit_source_fr_preprocessed.csv".format(data), shell=True)
-                with open("./reddit_source_fr_preprocessed.csv", 'a') as file:
-                    file.write(data+"\n")
-                file.close()
-                stats.ok += 1
-                if stats.total % 10000 == 0:
-                    print("=====================")
-                    end = time.time()
-                    print("Processed: " + str(stats.total) + "\n STATS : " + json.dumps(stats.__dict__) + "TIME : " + str((end-start)/60) + "min", flush=True)
-                    start = time.time()
-        else:
-            stats.empties += 1
-            continue
+            else:
+                break
 
 
 

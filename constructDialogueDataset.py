686545aaf8ff6c56329c57a55c8c456a545adf42
Aniss Bentebib <abentebib@asmodine.com>
2021-06-06 10:43:41 +0200

    constructDialogueDataset.py

--- constructDialogueDataset.py
+++ constructDialogueDataset.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import json
+import argparse
+
+k = dict()
+p = list()
+
+parser = argparse.ArgumentParser(description='Construction des données dialogues')
+parser.add_argument('--extractedPreprocessCsvFilePath', type=str, default="./",
+                    help='Path to the source file preprocessed by preprocess.py')
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    data = pd.read_csv(args.extractedPreprocessCsvFilePath, sep=";")
+    print(data.describe())
+    print(data.head())
